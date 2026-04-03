1627fa61a46153f940a7045a89b44c32e409ac84
wenqixwx-max <wenqixwx@gmail.com>
2026-04-03 10:32:21 +0800

    添加一个测试类用语测试资源加载

---
+++ src/main/java/com/zenith/asset/Test.java
@@ -0,0 +1,7 @@
+package com.zenith.asset;
+
+public class Test {
+    public static void main(String[] args) {
+        AssetResource assetResource = AssetResource.loadFromResources("textures/test.png");
+    }
+}
